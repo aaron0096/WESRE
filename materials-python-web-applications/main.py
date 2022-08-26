c9edf83118a05b4aabab39420e4e50234b1c4ea6
aaron0096 <109711683+aaron0096@users.noreply.github.com>
2022-08-26 17:05:23 +0800

    Update main.py

--- materials-python-web-applications/main.py
+++ materials-python-web-applications/main.py
@@ -30,13 +30,14 @@ def index():
         + "Fahrenheit: "
         + fahrenheit
     )
+app.logger.debug("debug log info")
+app.logger.info("Info log information")
+app.logger.warning("Warning log info")
+app.logger.error("Error log info")
+app.logger.critical("Critical log info")
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
-    app.logger.debug("debug log info")
-    app.logger.info("Info log information")
-    app.logger.warning("Warning log info")
-    app.logger.error("Error log info")
-    app.logger.critical("Critical log info")
+
 
